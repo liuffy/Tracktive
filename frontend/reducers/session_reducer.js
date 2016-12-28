a89433269a28114fa27346ac5cb088495240f242
Sophia Liu <sliu3@wellesley.edu>
2016-12-27 20:25:48 -0500

    Fixed

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,12 +1,9 @@
-import {RECEIVE_CURRENT_USER, 
-				RECEIVE_ERRORS,
-				RECEIVE_LIKE,
-				REMOVE_LIKE} from '../actions/session_actions';
+import {RECEIVE_CURRENT_USER, RECEIVE_ERRORS} from '../actions/session_actions';
 
-const _nullUser = Object.freeze({
-	currentUser: {likedTracktlists: []},
+const _nullUser = {
+	currentUser: null,
 	errors: []
-})
+}
 
 const SessionReducer = (state = _nullUser, action) =>{
 	Object.freeze(state);
@@ -19,18 +16,6 @@ const SessionReducer = (state = _nullUser, action) =>{
 		case RECEIVE_ERRORS:
 			const errors = action.errors;
 			return Object.assign({}, _nullUser, {errors})
-
-		case RECEIVE_LIKE:
-			let dupState = Object.assign({}, state);
-			dupState.currentUser.likedTracktlists.push(parseInt(action.like.tracktlistId));
-			return currentUser;
-
-		case REMOVE_LIKE:
-			dupState = Object.assign({}, state);
-			const removedLike = action.like;
-			const removeIdx = dupState.currentUser.likedTracktlists.indexOf(removedLike.tracktlistId)
-		  dupState.currentUser.likedTracktlists.splice(removeIdx, 1); // removes like from array
-			return currentUser;
 
 		default:
 			return state;
