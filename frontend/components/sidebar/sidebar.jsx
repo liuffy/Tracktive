372b3cc28f1c735bb29881284ad45f8fd7954b0e
Sophia Liu <sliu3@wellesley.edu>
2017-02-26 12:55:10 -0800

    Finished media queries

--- frontend/components/sidebar/sidebar.jsx
+++ frontend/components/sidebar/sidebar.jsx
@@ -5,20 +5,39 @@ import FormModal from '../modals/form_modal';
 
 const Sidebar = ({currentUser}) => (
 
-			<div className="sidebar">
-					<div><Link 
+	<div>
+			<ul className="fullscreen-sidebar">
+					<li><Link 
 					className="sidebar-button"
 					activeClassName="active-button-side"
-					to={'/browse'}>EXPLORE</Link></div>
+					to={'/browse'}>EXPLORE</Link></li>
 
-					<div className="sidebar-button"><FormModal></FormModal></div>
+					<li className="sidebar-button"><FormModal></FormModal></li>
 
-					<div><Link 
+					<li><Link 
 					className="sidebar-button"
 					activeClassName="active-button-side"
-					to={`/users/${currentUser.id}`}>MINE</Link></div>
+					to={`/users/${currentUser.id}`}>MINE</Link></li>
+			</ul>
+
+	<ul className="hamburger-dropdown"> 
+		<li>
+			<ul className="sidebar">
+					<li><Link 
+					className="sidebar-button ham"
+					activeClassName="active-button-side"
+					to={'/browse'}>EXPLORE</Link></li>
+
+					<li className="sidebar-button ham"><FormModal></FormModal></li>
+
+					<li><Link 
+					className="sidebar-button ham"
+					activeClassName="active-button-side"
+					to={`/users/${currentUser.id}`}>MINE</Link></li>
+			</ul>
+		</li>
+	</ul>
 			</div>
-		
 
 )
 
